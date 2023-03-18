7293e0e4be8f615df429b8b94e50843373fabe8e
Vidit Kushwaha <viditkushwaha530@gmail.com>
2023-03-18 11:43:59 +0530

    complete

--- inodes-backend/db.js
+++ inodes-backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // const mongoURI = "mongodb://0.0.0.0:27017/test"
 // const mongoURI = "mongodb+srv://vidit:9120991477@cluster0.fhxgkns.mongodb.net/test"
-const mongoURI="mongodb+srv://randomthings1155:jIKaKMkfS14qpAr1@cluster1.ec8uv0q.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI="mongodb+srv://randomthings1155:jIKaKMkfS14qpAr1@cluster1.ec8uv0q.mongodb.net/?retryWrites=true&w=majority";
 const connectToMongo = async() => {
    try{
       mongoose.connect(mongoURI,()=>{
